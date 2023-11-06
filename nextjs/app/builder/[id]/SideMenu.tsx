02b78948cd76bec39df0f6093e4eeded977985fb
Nash <98664443+NashNoman@users.noreply.github.com>
2023-11-06 12:28:27 +0300

    Added some styling to side menu

--- nextjs/app/builder/[id]/SideMenu.tsx
+++ nextjs/app/builder/[id]/SideMenu.tsx
@@ -1,5 +1,7 @@
 export default function SideMenu() {
   return (
-    <aside className="sticky w-1/6 border-r left-0 top-16">SideMenu</aside>
+    <aside className="h-full sticky w-1/6 p-3 left-0 top-14">
+      <div className="bg-secondary rounded-md p-3">SideMenu</div>
+    </aside>
   );
 }
