f19e377c3fe5d9b94cd61d6808bbe1d56e82c9a5
Nash <98664443+NashNoman@users.noreply.github.com>
2023-11-17 16:53:53 +0300

    Created builder types and exam dummy data files

---
+++ nextjs/temp/examData.ts
@@ -0,0 +1,12 @@
+const EXAMDATA = {
+  course: "Introduction to Computers",
+  courseCode: "CSCI200",
+  examType: "Test1",
+  examPercentage: 10,
+  sections: [
+    { type: "mcq", sectionGrade: 5, questions: [{}] },
+    { type: "tof", sectionGrade: 5, questions: [{}] },
+  ],
+};
+
+export default EXAMDATA;
