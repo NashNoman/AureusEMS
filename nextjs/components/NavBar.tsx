7036f4151848f2ddd355d86df71a0b93b20a2913
Nash <98664443+NashNoman@users.noreply.github.com>
2023-11-17 16:23:42 +0300

    Forgot to git add .

--- nextjs/components/NavBar.tsx
+++ nextjs/components/NavBar.tsx
@@ -1,6 +1,23 @@
 import SimpleLogo from "@/components/Logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export function BuilderNavBar() {
-  return <div>{<SimpleLogo className="h-9" />}</div>;
+  return (
+    <div className="h-12 px-2 border-b flex justify-between items-center">
+      <Link href="/" className="cursor-pointer">
+        {<SimpleLogo className="h-9" />}
+      </Link>
+
+      <div className="flex-grow h-full ml-2 border-x flex items-center">
+        <div className="px-2 flex flex-col justify-center gap-0 h-full">
+          <p className="font-semibold text-lg">Test1</p>
+          <p className="text-xs text-muted-foreground">CSCI200</p>
+        </div>
+      </div>
+
+      <Button variant="link">Logout</Button>
+    </div>
+  );
 }
