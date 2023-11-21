25d6868263f71307bb570be5bd109c4a0fb668ad
Nash <98664443+NashNoman@users.noreply.github.com>
2023-11-21 23:31:33 +0300

    Courses are now displayed from the database

--- nextjs/app/courses/page.tsx
+++ nextjs/app/courses/page.tsx
@@ -1,39 +1,16 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { dbConnect } from "@/lib/db";
+import Course from "@/models/Course";
+import User from "@/models/User";
 import { CheckCircle2, SearchIcon, XCircle } from "lucide-react";
 
-const columns = ["CODE", "TITLE", "INSTRUCTOR", "CLO"];
-const courses = [
-  {
-    id: "asdaf",
-    code: "CSCI100",
-    title: "Introduction",
-    instructor: "omar fakih",
-    clo: true,
-  },
-  {
-    id: "asdaf",
-    code: "CSCI100",
-    title: "Introduction",
-    instructor: "omar fakih",
-    clo: false,
-  },
-  {
-    id: "asdaf",
-    code: "CSCI100",
-    title: "Introduction",
-    instructor: "omar fakih",
-    clo: false,
-  },
-  {
-    id: "asdaf",
-    code: "CSCI100",
-    title: "Introduction",
-    instructor: "omar fakih",
-    clo: true,
-  },
-];
-
 export default async function CoursesHome() {
+  await dbConnect();
+  let courses = [];
+  courses = await Course.find({
+    dept: "655a421d8dbcc5c428aa18f4",
+  }).populate({ path: "instructor", model: User });
+
   return (
     <div>
       <header className="mb-8 px-20">
@@ -52,23 +29,24 @@ export default async function CoursesHome() {
         </div>
       </header>
       <h1 className="ml-20">Information Technology - CSIT</h1>
-      <ScrollArea>
-        <main className="mt-14">
-          <div className="flex text-xs text-muted-foreground justify-between pr-36 px-20 items-center h-10 border-b border-accent">
-            <div className="w-24">CODE</div>
-            <div className="flex-grow">TITLE</div>
-            <div className="w-64">INSTRUCTOR</div>
-            <div className="w-24 text-center">CLO</div>
-          </div>
+      <main className="mt-14">
+        <div className="flex text-xs text-muted-foreground justify-between pr-36 px-20 items-center h-10 border-b border-accent">
+          <div className="w-24">CODE</div>
+          <div className="flex-grow">TITLE</div>
+          <div className="w-64">INSTRUCTOR</div>
+          <div className="w-24 text-center">CLO</div>
+        </div>
+        <ScrollArea>
           <div>
             {courses.map((course) => {
+              const { id, code, title, instructor, clo } = course;
               return (
                 <div className="cursor-pointer flex text-sm justify-between px-20 pr-36 items-center h-16 border-b border-accent hover:bg-accent transition-colors">
-                  <div className="w-24">{course.code}</div>
-                  <div className="flex-grow">{course.title}</div>
-                  <div className="w-64">{course.instructor}</div>
+                  <div className="w-24">{code}</div>
+                  <div className="flex-grow">{title}</div>
+                  <div className="w-64">{`${instructor.firstName} ${instructor.lastName}`}</div>
                   <div className="w-24">
-                    {course.clo ? (
+                    {clo ? (
                       <CheckCircle2 className="m-auto w-5" />
                     ) : (
                       <XCircle className="m-auto h-5 text-destructive" />
@@ -78,8 +56,8 @@ export default async function CoursesHome() {
               );
             })}
           </div>
-        </main>
-      </ScrollArea>
+        </ScrollArea>
+      </main>
     </div>
   );
 }
