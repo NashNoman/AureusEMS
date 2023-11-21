109f103f65450a611bc904e7cc9723c07c2e52f9
Nash <98664443+NashNoman@users.noreply.github.com>
2023-11-21 11:50:39 +0300

    Updated layout.tsx and added courses page.tsx

---
+++ nextjs/app/courses/page.tsx
@@ -0,0 +1,7 @@
+export default async function CoursesHome() {
+  return (
+    <main>
+      <div>Hello</div>
+    </main>
+  );
+}
--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,13 +1,28 @@
 import "./globals.css";
+import NavBar from "@/components/NavBar";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "Aureus",
 };
 
-export default function RootLayout({ children }) {
+const inter = Inter({ subsets: ["latin"] });
+
+type Props = { children: ReactNode };
+
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={
+          "h-screen dark bg-background text-primary flex " + inter.className
+        }
+      >
+        <NavBar />
+        <ScrollArea className="h-full w-full pt-14">{children}</ScrollArea>
+      </body>
     </html>
   );
 }
