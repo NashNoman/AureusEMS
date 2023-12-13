00afc56e9aa85d299a653a6d0650b2c4120269f0
Nash <98664443+NashNoman@users.noreply.github.com>
2023-12-13 22:33:31 +0300

    Refactore homepage table into a client component

--- nextjs/app/(main)/TableController.tsx
+++ nextjs/app/(main)/TableController.tsx
@@ -3,21 +3,25 @@
 import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 
-export default function TableController() {
-  const [display, setDisplay] = useState<"all" | "recent">("all");
-
+export default function TableController({
+  onChange,
+  display,
+}: {
+  onChange: (display: "all" | "recent") => void;
+  display: "all" | "recent";
+}) {
   return (
     <div className="px-20 mb-6">
       <Button
         variant={(display === "all" && "secondary") || "ghost"}
         className="mr-4"
-        onClick={() => setDisplay("all")}
+        onClick={() => onChange("all")}
       >
         All
       </Button>
       <Button
         variant={(display === "recent" && "secondary") || "ghost"}
-        onClick={() => setDisplay("recent")}
+        onClick={() => onChange("recent")}
       >
         Recent
       </Button>
--- nextjs/app/(main)/page.tsx
+++ nextjs/app/(main)/page.tsx
@@ -1,10 +1,5 @@
-import TableController from "@/app/(main)/TableController";
+import HomepageTable from "@/app/(main)/HomepageTable";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
-import DataTableBody from "@/components/DataTable/DataTableBody";
-import DataTableHeader from "@/components/DataTable/DataTableHeader";
-import DataTableItem from "@/components/DataTable/DataTableItem";
-import DataTableRow from "@/components/DataTable/DataTableRow";
-import { Button } from "@/components/ui/button";
 import { ColumnWidth } from "@/enums";
 import { dbConnect } from "@/lib/db";
 import Course from "@/models/Course";
@@ -51,106 +46,7 @@ export default async function Home() {
 
   return (
     <main>
-      <TableController />
-      <DataTableHeader columns={columns} />
-      <DataTableBody>
-        {exams.map((exam: any) => {
-          const { id, type, percentage, doc, courseTitle, courseCode } = exam;
-
-          return (
-            <DataTableRow key={id}>
-              <DataTableItem columnWidth="sm">{courseCode}</DataTableItem>
-              <DataTableItem>{courseTitle}</DataTableItem>
-              <DataTableItem columnWidth="sm" center>
-                {type}
-              </DataTableItem>
-              <DataTableItem columnWidth="sm" center>
-                {percentage + "%"}
-              </DataTableItem>
-              {doc ? (
-                <>
-                  <DataTableItem columnWidth="sm" center>
-                    {doc.status}
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    {doc.due}
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    {doc.updatedAt}
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    {doc.createdAt}
-                  </DataTableItem>
-                </>
-              ) : (
-                <>
-                  <DataTableItem columnWidth="sm" center>
-                    -
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    -
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    -
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    -
-                  </DataTableItem>
-                </>
-              )}
-            </DataTableRow>
-          );
-        })}
-        {/* {exams.map((exam: (typeof exams)[0]) => {
-          const { id, status, title, course, due, updatedAt, createdAt } = exam;
-
-          return (
-            <DataTableRow key={id}>
-              <DataTableItem columnWidth="sm">{status}</DataTableItem>
-              <DataTableItem columnWidth="sm">{title}</DataTableItem>
-              <DataTableItem>{course.title}</DataTableItem>
-              <DataTableItem columnWidth="sm" center>
-                {due}
-              </DataTableItem>
-              <DataTableItem columnWidth="sm" center>
-                {updatedAt}
-              </DataTableItem>
-              <DataTableItem columnWidth="sm" center>
-                {createdAt}
-              </DataTableItem>
-            </DataTableRow>
-          );
-        })}
-        {courses.map((course: (typeof courses)[0]) => {
-          const { id, code, title, instructor: inst, exams } = course;
-          const e = exams.filter((exam: any) => !exam.doc);
-          if (e.length)
-            return e.map((exam: any) => {
-              return (
-                <DataTableRow key={exam.id}>
-                  <DataTableItem columnWidth="sm">{code}</DataTableItem>
-                  <DataTableItem>{course.title}</DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    {exam.type}
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    -
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    -
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    -
-                  </DataTableItem>
-                  <DataTableItem columnWidth="sm" center>
-                    -
-                  </DataTableItem>
-                </DataTableRow>
-              );
-            });
-          else return null;
-        })} */}
-      </DataTableBody>
+      <HomepageTable columns={columns} examsData={exams} />
     </main>
   );
 }
