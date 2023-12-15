dec470db7876d19a56b6eea02ce2c04bfef40743
Nash <98664443+NashNoman@users.noreply.github.com>
2023-12-15 22:55:10 +0300

    Set flask's initial files

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,8 @@
 import configparser
 import os
 from flask import Flask
+from flask_smorest import Api
+from resources.clos import blp as clos_blp
 
 
 app = Flask(__name__)
@@ -10,6 +12,9 @@ config.read(os.path.abspath(os.path.join("config.ini")))
 
 app.config["MONGO_URI"] = config["PROD"]["DB_URI"]
 
+api = Api(app)
+
+api.register_blueprint(clos_blp)
 
 if __name__ == "__main__":
     app.run(host="192.168.1.1", port=4000, debug=True)
